eb51d6a203d93c0ffe1633985fecad9905648c5e
Kirill "Zkir <mekillot@yandex.ru>
2022-12-13 19:37:06 +0300

    linux zip/unzip

---
+++ tools/update_ckan.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# curl -d @$1 -H "Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzA2MTE0NjMsImp0aSI6ImtJUTcySHpfS1pWRXY4dmd4bXRkdmc5d0sxVGZQVVEzYVhmcmNMTEZWc3dPZXp1WGx0dFI2UkdEdHBDNFE0UC14OER1MkUxc3d4S2J0NFBXIn0.dL3nbtGCtSs8fpnmoFOSt0j_oXcDhU16M1jw7FQS3xI" -X POST "https://geocint.kontur.io/ckan/api/3/action/package_create"
+# curl -d @$1 -H "Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzA2MTE0NjMsImp0aSI6ImtJUTcySHpfS1pWRXY4dmd4bXRkdmc5d0sxVGZQVVEzYVhmcmNMTEZWc3dPZXp1WGx0dFI2UkdEdHBDNFE0UC14OER1MkUxc3d4S2J0NFBXIn0.dL3nbtGCtSs8fpnmoFOSt0j_oXcDhU16M1jw7FQS3xI" -X POST "https://geocint.kontur.io/ckan/api/3/action/package_update"
+
+
+
+
